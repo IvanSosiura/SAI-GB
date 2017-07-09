35f8b2828b86657e58cfe410dd31767ecb37a4b8
IvanSosiura <ivansosiura@gmail.com>
2017-07-09 21:16:38 +0300

    firs commit
    first version

---
+++ js/index.js
@@ -0,0 +1,20 @@
+//SLIDER TWO-WAY
+var galleryTop = new Swiper('.gallery-top', {
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        spaceBetween: 12,
+        slidesPerView: 1.9,
+        centeredSlides: true,
+    });
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+        spaceBetween: 10,
+        
+        centeredSlides: true,
+        
+        slidesPerView: 'auto',
+        touchRatio: 0.2,
+        slideToClickedSlide: true
+    });
+    galleryTop.params.control = galleryThumbs;
+    galleryThumbs.params.control = galleryTop;
+
